6447ab426f0d9f1a3d61cbf5b0fa588c9fff402b
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-02-01 15:20:55 -0800

    Defined schema for a repo.

--- database/index.js
+++ database/index.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/gitrepos');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: {
+    type: Number,
+    index: true, //makes scanning quicker
+    unique: true //prevents duplicate copies of a repo.
+  },
+  full_name: String,
+  url: String,
+  owner_name: String,
+  owner_url: String,
+  size: Number,
+  createdAt: Date,
+  updatedAt: Date,
+  description: String,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
